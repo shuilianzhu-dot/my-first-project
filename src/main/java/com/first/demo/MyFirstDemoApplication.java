04fbaac9eb0238fbdca1fc1e04a41debc1f8ffbc
czk <czk@s.com>
2020-01-08 10:45:31 +0800

    first import

---
+++ src/main/java/com/first/demo/MyFirstDemoApplication.java
@@ -0,0 +1,13 @@
+package com.first.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstDemoApplication.class, args);
+    }
+
+}
